e181d477a87529cb1fe583a778ba229305ecfe50
maheshacharya <m.acharya@onehippo.com>
2016-09-14 05:58:16 -0400

    gameday controlpanel perspective removed

--- cms/src/main/resources/com/dev9/hippo/HippoConnectPerspective.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".button-help").hover(function () {
-        var content = $(this).find(".help-content");
-        $(content).fadeIn();
-    }, function () {
-        var content = $(this).find(".help-content");
-        $(content).fadeOut();
-    });
-});
